73118d6fb36fc89e9ee7e2c44dd7a53078480000
Archita Maurya <79247686+meta-archita-maurya@users.noreply.github.com>
2021-03-16 11:56:17 +0530

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,129 +1,67 @@
-package stackImp;
+package arrayOperation;
 
 import java.util.Scanner;
 
 public class MainClass {
-
 	
+	static int [] array;
+	
+	//takes the array Input from the user
+	static void getArrayInput(Scanner sc){
+		System.out.println("Enter size of Array");
+		int size = sc.nextInt();
+		array = new int[size];
+		System.out.println("Enter elements of Array");
+		for(int index=0;index<size;index++){
+			array[index] = sc.nextInt();
+		}
+	}
+
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Expression :");
-		String exp = sc.nextLine();
-		System.out.println("Ans :"+evaluate(exp));
+		ArrayOperation arrayOp = new ArrayOperation();
+		while(true){
+			System.out.println("1.largest mirror in array is of length :");
+			System.out.println("2.Number of clumps in array");
+			System.out.println("3.fixXY");
+			System.out.println("4.Find split Array Index");
+			System.out.println("5.Exit");
+			int choice =  sc.nextInt();
+			switch(choice){
+			case 1:
+				getArrayInput(sc);
+				System.out.println("\nAns :"+arrayOp.largestMirror(array)+"\n");
+				break;
+			case 2:
+				getArrayInput(sc);
+				System.out.println("\nAns :"+arrayOp.countClump(array)+"\n");
+				break;
+			
+			case 3:
+				getArrayInput(sc);
+				System.out.println("Enter x");
+				int x = sc.nextInt();
+				System.out.println("Enter y");
+				int y = sc.nextInt();
+				displayArray();
+				break;
+			case 4:
+				getArrayInput(sc);
+				System.out.println("\nAns :"+arrayOp.getSplitIndex(array)+"\n");
+				break;
+			default:
+				return;
+				}
+		}
 	}
-	
-	
-	public static int evaluate(String expression)
-    {
-        char[] tokens = expression.toCharArray();
- 
-         // Stack for numbers: 'values'
-        MyStack values = new MyStack();
- 
-        // Stack for Operators: 'ops'
-        MyStack ops = new MyStack();
- 
-        for (int i = 0; i < tokens.length; i++)
-        {
-             
-            // Current token is a 
-            // whitespace, skip it
-            if (tokens[i] == ' ')
-                continue;
- 
-            // Current token is a number, 
-            // push it to stack for numbers
-            if (tokens[i] >= '0' && 
-                 tokens[i] <= '9')
-            {
-                StringBuffer sbuf = new
-                            StringBuffer();
-                 
-                // There may be more than one 
-                // digits in number
-                while (i < tokens.length && 
-                        tokens[i] >= '0' && 
-                          tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
-                values.push(Integer.parseInt(sbuf.
-                                      toString()));
-                  i--;
-            }
- 
-            // Current token is an opening brace, 
-            // push it to 'ops'
-            else if (tokens[i] == '(')
-                ops.push(tokens[i]);
- 
-            // Closing brace encountered, 
-            // solve entire brace
-            else if (tokens[i] == ')')
-            {
-                while (ops.opPeek() != '(')
-                  values.push(applyOp(ops.opPop(), 
-                                   values.intPop(), 
-                                 values.intPop()));
-                ops.opPop();
-            }
- 
-            // Current token is an operator.
-            else if (tokens[i] == '+' || 
-                     tokens[i] == '-' ||
-                     tokens[i] == '*' || 
-                        tokens[i] == '/')
-            {
-                while (!ops.isEmpty() && hasPrecedence(tokens[i],ops.opPeek()))
-                		values.push(applyOp(ops.opPop(),values.intPop(),values.intPop()));
- 
-                // Push current token to 'ops'.
-                ops.push(tokens[i]);
-            }
-        }
- 
-        // Entire expression has been 
-        // parsed at this point, apply remaining
-        // ops to remaining values
-        while (!ops.isEmpty())
-            values.push(applyOp(ops.opPop(), 
-                             values.intPop(), 
-                           values.intPop()));
- 
-        // Top of 'values' contains 
-        // result, return it
-        return values.intPop();
-    }
- 
-    // Returns true if 'op2' has higher 
-    // or same precedence as 'op1',
-    // otherwise returns false.
-    public static boolean hasPrecedence(char op1, char op2)
-    {
-        if (op2 == '(' || op2 == ')')
-            return false;
-        if ((op1 == '*' || op1 == '/') && 
-            (op2 == '+' || op2 == '-'))
-            return false;
-        else
-            return true;
-    }
-	 public static int applyOp(char op,int b, int a) {
-		 
-		 switch (op){
-		 	case '+':
-		 		return a + b;
-		 	case '-':
-		 		return a - b;
-		 	case '*':
-		 		return a * b;
-		 	case '/':
-		 		if (b == 0)
-		 			throw new
-		 			UnsupportedOperationException(
-		 					"Cannot divide by zero");
-		 		return a / b;
-		 }
-		 return 0;
-	 	}
+
+	//display the array
+	private static void displayArray() {
+		int index = -1;
+		while(++index<array.length){
+			System.out.print(array[index]+" ");
+		}
+		System.out.println();
+		
+	}
 }
-	 
-	 
