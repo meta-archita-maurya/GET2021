f068cb5bf5d4accc5f79e6bce84a8e063a018d8a
Archita Maurya <79247686+meta-archita-maurya@users.noreply.github.com>
2021-03-20 23:42:33 +0530

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,17 @@
+package dictionary;
+public class Entry {
+    public String key;
+    public String value;
+    Entry left,right;
+    public Entry(String key,String value){
+        this.key=key;
+        this.value=value;
+        left=right=null;
+    }
+    public String getKey(){
+        return key;
+    }
+    public String getValue(){
+        return value;
+    }
+}
